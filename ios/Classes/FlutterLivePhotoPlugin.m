ccfc489fd313ec1f9d0848f3afd89e3b6acd13a0
wangbangpc <79408854@163.com>
2020-12-03 12:03:43 +0800

    本地提交

--- ios/Classes/FlutterLivePhotoPlugin.m
+++ ios/Classes/FlutterLivePhotoPlugin.m
@@ -5,7 +5,6 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_live_photo-Swift.h"
 #import "OcFlutterLivePhotoPlugin.h"
 #endif
 
